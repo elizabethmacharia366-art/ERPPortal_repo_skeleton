0f901ffacccae28d41f9b620cde2f319a0de6e4d
elizabethmacharia366-art <elizabethmacharia366@gmail.com>
2026-08-26 10:10:38 +0300

    Add Serilog structured logging to console and rolling file

--- ERPPortal/backend/ERPPortal.API/Program.cs
+++ ERPPortal/backend/ERPPortal.API/Program.cs
@@ -1,7 +1,16 @@
 using ERPPortal.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/erpportal-.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
